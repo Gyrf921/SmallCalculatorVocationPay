87cbbb525ee08e58df911ff17df0865c136270be
Oladushek <gyrf921@gmail.com>
2023-05-16 11:24:32 +0300

    Refactoring code

--- src/main/java/com/example/neoflex/controllers/CalculatorController.java
+++ src/main/java/com/example/neoflex/controllers/CalculatorController.java
@@ -15,52 +15,34 @@ import java.util.*;
 @RestController
 public class CalculatorController {
 
-    private static final Set<LocalDate> nonWorkingHolidays = new HashSet<>();
-    private static void fillHolidaysArray()
-    {
-        nonWorkingHolidays.addAll(Arrays.asList(
-                LocalDate.of(2023 , 1 , 1 ),
-                LocalDate.of(2023 , 1 , 2 ),
-                LocalDate.of(2023 , 1 , 3 ),
-                LocalDate.of(2023 , 1 , 4 ),
-                LocalDate.of(2023 , 1 , 5 ),
-                LocalDate.of(2023 , 1 , 6 ),
-                LocalDate.of(2023 , 1 , 7 ),
-                LocalDate.of(2023 , 1 , 8 ),
-                LocalDate.of( 2023 , 2 , 23 ),
-                LocalDate.of( 2023 , 3 , 8 ),
-                LocalDate.of( 2023 , 5 , 1 ),
-                LocalDate.of( 2023 , 5 , 9 ),
-                LocalDate.of( 2023 , 6 , 12 ),
-                LocalDate.of( 2023 , 4 , 11 )));
-    }
+    private static final Set<LocalDate> nonWorkingHolidays = new HashSet<>(Arrays.asList(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 2), LocalDate.of(2023, 1, 3), LocalDate.of(2023, 1, 4), LocalDate.of(2023, 1, 5), LocalDate.of(2023, 1, 6), LocalDate.of(2023, 1, 7), LocalDate.of(2023, 1, 8), LocalDate.of(2023, 2, 23), LocalDate.of(2023, 3, 8), LocalDate.of(2023, 5, 1), LocalDate.of(2023, 5, 9), LocalDate.of(2023, 6, 12), LocalDate.of(2023, 4, 11)));
+    private static final int maxHolidayYear = 2023;
+    private static final BigDecimal countDaysInMonth = BigDecimal.valueOf(30.0);
 
     @GetMapping("/calculate")
     public CalculationResponse calculateVacationPay(@RequestParam(value = "salary") BigDecimal salary,
-                                                    @RequestParam(value = "startDate") String startDateParam,
+                                                    @RequestParam(value = "startDate" ) String startDateParam,
                                                     @RequestParam(value = "endDate") String endDateParam) {
-        LocalDate startDate = LocalDate.parse( startDateParam);
+        LocalDate startDate = LocalDate.parse(startDateParam);
         //plusDays is necessary so that the program takes into account the last day (passed in the parameter)
         LocalDate endDate = LocalDate.parse(endDateParam).plusDays(1);
 
-        fillHolidaysArray();
-        BigDecimal countDaysInMonth = BigDecimal.valueOf(30.0);
-        long countAllDays = startDate.until(endDate, ChronoUnit.DAYS);
-        long countHoliday = 0;
+        int countAllDays = (int)startDate.until(endDate, ChronoUnit.DAYS);
+        int countHoliday = 0;
         Calendar calendarForCheckingWeekends = new GregorianCalendar();
         List<LocalDate> allDatesBetweenStartAndEndDate = startDate.datesUntil(endDate).toList();
 
-        for (LocalDate localDate : allDatesBetweenStartAndEndDate){
-            if (localDate.getYear() > 2023) {
+        for (LocalDate localDate : allDatesBetweenStartAndEndDate) {
+            if (localDate.getYear() > maxHolidayYear) {
                 throw new UnsupportedOperationException("this year more than contains in nonWorkingHolidays arrays");
             }
-            if (nonWorkingHolidays.contains(localDate)){
+            if (nonWorkingHolidays.contains(localDate)) {
                 countHoliday++;
                 continue;
             }
 
             calendarForCheckingWeekends.setTime(Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
-            if(calendarForCheckingWeekends.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || calendarForCheckingWeekends.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY){
+            if (calendarForCheckingWeekends.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || calendarForCheckingWeekends.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
                 countHoliday++;
             }
         }
